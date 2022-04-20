4d884f71e3e3c507c0ad87f7f9b47f9e7ff5171f
Dongd_Zhou <dongdong.zhou@xdxct.com>
2022-04-20 18:06:41 +0800

    init

---
+++ src/main/java/cn/zm1001/upload/UploaderApplication.java
@@ -0,0 +1,16 @@
+package cn.zm1001.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dongd_Zhou
+ * @desc 启动类
+ * @date 2022/4/20
+ */
+@SpringBootApplication
+public class UploaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UploaderApplication.class, args);
+    }
+}
